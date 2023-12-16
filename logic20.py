7f5d07a32a145ab133e90c389b78a3e3e881af38
Abduvaliyevakumushxon <shodmonovd1985@gmail.com>
2023-12-16 15:32:14 +0500

    20-vazifa tugadi

--- logic20.py
+++ logic20.py
@@ -8,5 +8,10 @@ def main(n):
     Returns:
         bool: answer
     """
-
-    return
+    x1=n%10
+    x2=n//10%10
+    x3=n//100%10
+    x4=n//1000%10
+    x5=n//10000
+    return x1+x2+x3+x4+x5>=3
+print(main(10011))
